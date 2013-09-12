08ca94a1f6cdf5f83bb9210090842dd2fbf0f8e4
Ankit Mahato <aryan.9001@gmail.com>
2013-09-12 22:46:37 +0530

    Updated solver with correct output directory.

--- examples/phase_change/phase_change_solver/brent_test.py
+++ examples/phase_change/phase_change_solver/brent_test.py
@@ -1,9 +1,16 @@
-from pcsolver import SolidiPy
+from pcsolver import PhaseChangeSolver
+import sys
+import os
+cwd = os.path.split(os.path.join(os.getcwd(), __file__))[0]
+
+output_dir = os.path.join(cwd, 'output')
+if not os.path.exists(output_dir):
+	os.makedirs(output_dir)	
 
 grid={'xLength':9.00E-2, 
       'yLength':6.35E-2, 
-      'xGrids':42, 
-      'yGrids':32}  
+      'xGrids':22, 
+      'yGrids':16}  
               
 material={'darcyConstant':1.6E6,
           'density':6093.0,   
@@ -17,19 +24,22 @@ material={'darcyConstant':1.6E6,
 
 ics={'TINITIAL':28.3} 
 bcs={'THOT':38} 
-times={'steps':({'starttime':0,'timeStep':5,'maxIter':40},
+times={'steps':({'starttime':0,'timeStep':5,'maxIter':20},
                 {'starttime':40,'timeStep':10,'maxIter':100},
                 {'starttime':150,'timeStep':10,'maxIter':1000}),               
-      'timeLast':600.0,
+      'timeLast':180.0,
       'printInterval':20}
 
-a = SolidiPy(grid,material,ics,bcs,times)
+a = PhaseChangeSolver(grid,material,ics,bcs,times)
 a.grid_initialize()
 a.geometry_initialize()
 a.start()
+
 while not a.LSTOP:  
   a.dense()  
   a.bound()   
   a.oldval() 
   a.coeff()  
+  a.vect_plot() 
+  a.printout(output_dir)
 print("PROGRAM FINISHED")  
